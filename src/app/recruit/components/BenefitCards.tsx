e4f4a04806244c66b8ec0ae3327eca36f13fb747
이준영 <ff1451@gmail.com>
2025-08-20 21:14:47 +0900

    feat: 혜택 카드 구현

---
+++ src/app/recruit/components/BenefitCards.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+import { type Benefit, BENEFITS } from '@/static/recruit/benefit';
+
+function BenefitCard({ title, descriptions, Icon, offset }: Benefit) {
+  return (
+    <div
+      className={clsx(
+        'h-100 w-70 rounded-[20px] bg-[linear-gradient(180deg,#F5DBFF_0%,#F9E8FF_100%)] pt-8',
+        'flex flex-col items-center',
+        offset && 'translate-y-8',
+      )}
+    >
+      <div className="text-primary-100 text-2xl font-medium">{title}</div>
+      <div className="mt-1.5 pb-25 text-center text-[17px] leading-[150%] whitespace-pre-line text-neutral-200">
+        {descriptions}
+      </div>
+      <Icon />
+    </div>
+  );
+}
+
+export default function BenefitCards() {
+  return (
+    <div className="mt-70">
+      <div className="title">
+        <span className="font-bold">궤도</span>에 오르면
+        <div>얻을 수 있는 것들</div>
+      </div>
+
+      <div className="mt-21 flex gap-4.5">
+        {BENEFITS.map((benefit) => (
+          <BenefitCard key={benefit.title} {...benefit} />
+        ))}
+      </div>
+    </div>
+  );
+}
