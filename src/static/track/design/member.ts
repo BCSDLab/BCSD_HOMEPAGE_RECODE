3ba991e7f5352ae5182f8e2bda2c31d3417eef2f
이준영 <ff1451@gmail.com>
2025-08-22 10:17:26 +0900

    feat: 트랙 멤버 타입 및 정적 데이터 추가

---
+++ src/static/track/design/member.ts
@@ -0,0 +1,72 @@
+import type { Member } from '@/types/trackMember';
+
+const DESIGN_MEMBER: Member[] = [
+  {
+    id: 1,
+    name: '곽의신',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/euisin.jpg',
+  },
+  {
+    id: 2,
+    name: '최주원',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/juwon.jpg',
+  },
+  {
+    id: 3,
+    name: '김채은',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/chaeeun.png',
+  },
+  {
+    id: 4,
+    name: '이준서',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/junseo.jpg',
+  },
+  {
+    id: 5,
+    name: '김다준',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/dajun.jpg',
+  },
+  {
+    id: 6,
+    name: '김두언',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/duun.png',
+  },
+  {
+    id: 7,
+    name: '최성일',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/seongil.jpg',
+  },
+  {
+    id: 8,
+    name: '박소영',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/soyoung.png',
+  },
+  {
+    id: 9,
+    name: '최두완',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/doowan.png',
+  },
+  {
+    id: 10,
+    name: '장교은',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/gyoeun.png',
+  },
+  {
+    id: 11,
+    name: '곽지연',
+    role: 'REGULAR',
+    image: 'https://image.bcsdlab.com/jiyeon.png',
+  },
+];
+
+export default DESIGN_MEMBER;
