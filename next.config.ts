641098e18126e98a486aff8c831aa23be3aa8d2b
이준영 <ff1451@gmail.com>
2026-01-05 04:44:18 +0900

    chore: 리액트 컴파일러, turbopack 설정

--- next.config.ts
+++ next.config.ts
@@ -3,35 +3,26 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
+  reactCompiler: true,
+
   images: {
     deviceSizes: [640, 768, 1024, 1280, 1440, 1600, 1920],
     formats: ['image/avif', 'image/webp'],
-
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.bcsdlab.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.koreatech.in',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.slack-edge.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ca.slack-edge.com',
-      },
+      { protocol: 'https', hostname: 'image.bcsdlab.com' },
+      { protocol: 'https', hostname: 'static.koreatech.in' },
+      { protocol: 'https', hostname: 'avatars.slack-edge.com' },
+      { protocol: 'https', hostname: 'ca.slack-edge.com' },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
-    return config;
+
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
 };
 
